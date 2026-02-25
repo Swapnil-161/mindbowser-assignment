7528e7d108792dceb944d2d9ea91a7938baab2c3
Swapnil-161 <swapnildaware1612@gmail.com>
2026-02-26 00:33:49 +0530

    Final

--- frontend/src/components/RichTextEditor.jsx
+++ frontend/src/components/RichTextEditor.jsx
@@ -1,5 +1,5 @@
 import ReactQuill from 'react-quill-new';
-import 'react-quill/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.snow.css';
 
 const RichTextEditor = ({ value, onChange }) => {
     const modules = {
